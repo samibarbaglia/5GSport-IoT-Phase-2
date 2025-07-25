a112d5004bbdf877b4d8ec683fa244e16da9249f
samibarbaglia <sofia.barbaglia@metropolia.fi>
2025-07-25 14:25:08 +0300

    GNSS update

--- picoW-app/config.py
+++ picoW-app/config.py
@@ -1,9 +1,14 @@
-#Switch Pin
+# Switch Pin
 SW_0_PIN = 9
 SW_1_PIN = 8
 SW_2_PIN = 7
 
-#Led Pin
+# Led Pin
 LED1 = 22
 LED2 = 21
 LED3 = 20
+
+# UART Pin
+TX_PIN = 4
+RX_PIN = 5
+UART_BAUD_RATE = 115200
